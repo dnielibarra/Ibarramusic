afae5a6b6e7af3b66bc5f1c1e82cc184bb2c8791
dnielibarra <65689515+dnielibarra@users.noreply.github.com>
2026-06-25 23:26:07 -0600

    Actualizar rock.js

--- rock.js
+++ rock.js
@@ -1,1 +1,15 @@
-h
+const rock = [
+  {
+    titulo: "Ejemplo Rock",
+    autor: "Ibarra Music",
+    tono: "Am",
+    etiquetas: ["Rock", "Ensayo"],
+    letra: `
+[Am]Riff inicial
+[F]entra la guitarra
+[C]luego la voz
+[G]y sube la canción.
+`,
+    notas: "La guitarra entra sola al principio."
+  }
+];
