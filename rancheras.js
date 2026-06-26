971ef44057467a79f448c71399531e176d589c3e
dnielibarra <65689515+dnielibarra@users.noreply.github.com>
2026-06-25 23:25:33 -0600

    Actualizar rancheras.js

--- rancheras.js
+++ rancheras.js
@@ -1,1 +1,14 @@
-j
+const rancheras = [
+  {
+    titulo: "Ejemplo Ranchera",
+    autor: "Tradicional",
+    tono: "C",
+    etiquetas: ["Ranchera", "Familiar", "Fiesta"],
+    letra: `
+[C]Aquí va la letra
+[G]de una ranchera
+[F]con mucho sentimiento
+[C]para cantar.
+`
+  }
+];
