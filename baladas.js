3f5927f464f93331527f2d184748be0ffcd0492f
dnielibarra <65689515+dnielibarra@users.noreply.github.com>
2026-06-25 23:23:09 -0600

    Actualizar baladas.js

--- baladas.js
+++ baladas.js
@@ -1,1 +1,23 @@
-r
+const baladas = [
+  {
+    titulo: "Ejemplo Balada",
+    autor: "Ibarra Music",
+    tono: "G",
+    etiquetas: ["Balada", "Familiar"],
+    letra: `
+[G]Esta es una canción de ejemplo
+[D]para probar Ibarra Music
+[Em]con acordes y etiquetas
+[C]muy fácil de encontrar.
+`,
+    tablatura: `
+e|-----------------|
+B|-----------------|
+G|-------0---------|
+D|---0-2---2-0-----|
+A|-3-----------3---|
+E|-----------------|
+`,
+    notas: "Aquí puedes poner indicaciones para ensayo."
+  }
+];
