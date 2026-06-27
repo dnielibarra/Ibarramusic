455f06553a1dfb038d4c9904be3f4394521e05ba
dnielibarra <65689515+dnielibarra@users.noreply.github.com>
2026-06-27 07:58:24 -0600

    Actualizar baladas.js

--- baladas.js
+++ baladas.js
@@ -1,14 +1,52 @@
 const baladas = [
   {
-    titulo: "Ejemplo Balada",
-    autor: "Ibarra Music",
-    tono: "G",
-    etiquetas: ["Balada", "Familiar"],
+    titulo: "Y volvere <Emporte moi>",
+    autor: "<Alain Barriere,> Germain de lamfiente",
+    tono: "Am",
+    etiquetas: ["Balada", "Familiar, "1969)],
     letra: `
-Est[G]a es una canción de ejemplo
-[D]para probar Ibarra Music
-[Em]con acordes y etiquetas
-[C]muy fácil de encontrar.
+Amor, adiós
+No se puede continuar
+Ya la magia termino
+Ahora tengo que marchar
+Será mejor
+Seguir nuestra soledad
+Si hoy el cielo se cubrió
+Quizás mañana brille el sol
+No sufras más
+Quizás mañana
+nuestro llanto quede atrás
+Y si me dices
+que tu amor me esperará
+Tendré la luz
+que mi sendero alumbrará
+Y volveré
+Como un ave
+que retorna a su nidal
+Veras que pronto
+volveré y me quedaré
+Por esa paz que siempre,
+siempre tú me das
+Que tú me das
+No sufras más
+Quizás mañana
+nuestro llanto quede atrás
+Y si me dices
+que tu amor me esperará
+Tendré la luz
+que mi sendero alumbrará
+Y volveré
+Como un ave
+que retorna a su nidal
+Veras que pronto
+volveré y me quedaré
+Por esa paz que siempre,
+siempre tú me das
+Que tú me das
+Y volveré
+A tus brazos caeré
+Las estrellas brillaran
+Nuestro amor, renacerá.
 `,
     tablatura: `
 e|-----------------|
