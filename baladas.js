6b14541c725a507c57050db6a58c7bdca630a9d3
dnielibarra <65689515+dnielibarra@users.noreply.github.com>
2026-06-27 17:13:23 -0600

    Actualizar baladas.js

--- baladas.js
+++ baladas.js
@@ -1,66 +1,11 @@
 const baladas = [
   {
-    titulo: "Y volvere",
-    autor: "Los Angeles Negros",
-    tono: "Am",
-    etiquetas: ["Balada", "Familiar", "(1969)"],
-    letra: `
-Amor, adiós
-No se puede continuar
-Ya la magia termino
-Ahora tengo que marchar
-Será mejor
-Seguir nuestra soledad
-Si hoy el cielo se cubrió
-Quizás mañana brille el sol
-No sufras más
-Quizás mañana
-nuestro llanto quede atrás
-Y si me dices
-que tu amor me esperará
-Tendré la luz
-que mi sendero alumbrará
-Y volveré
-Como un ave
-que retorna a su nidal
-Veras que pronto
-volveré y me quedaré
-Por esa paz que siempre,
-siempre tú me das
-Que tú me das
-No sufras más
-Quizás mañana
-nuestro llanto quede atrás
-Y si me dices
-que tu amor me esperará
-Tendré la luz
-que mi sendero alumbrará
-Y volveré
-Como un ave
-que retorna a su nidal
-Veras que pronto
-volveré y me quedaré
-Por esa paz que siempre,
-siempre tú me das
-Que tú me das
-Y volveré
-A tus brazos caeré
-Las estrellas brillaran
-Nuestro amor, renacerá.
-`,
-    tablatura: `
-e|-----------------|
-B|-----------------|
-G|-------0---------|
-D|---0-2---2-0---0-|
-A|-3-----------3---|
-E|-----------------|
-`,
+
 
     notas: "Está es una canción italiana que se llama Emporte moi compuesta por Alain Barriere que después Germain escribió la letra en español.."
   },
 {
-  titulo: "Y volvere v1.2",
+  titulo: "Y volvere",
   autor: "Germain de la fuente",
   tono: "Am",
   etiquetas: ["Balada", "Familiar", "(1969)"],
@@ -120,12 +65,12 @@ A|-3-----------3---|
 E|-----------------|
 `,
   solo: `
-e|-----------------|
-B|-----------------|
-G|-------0---------|
-D|---0-2---2-0---0-|
-A|-3-----------3---|
-E|-----------------|
+e|--------------------------|
+B|--------------------------|
+G|-------0------------------|
+D|---0-2---2-0---0----------|
+A|-3-----------3---3--2--3--|
+E|--------------------------|
 `,
   historia: `...`
 }
